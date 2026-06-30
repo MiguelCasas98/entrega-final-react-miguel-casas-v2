1865afe2ec2bdf1de9601e8843e7810342edce56
Miguel Casas <casasmiventas@gmail.com>
2026-06-30 18:42:44 -0300

    Build final funcionando

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/entrega-final-react-miguel-casas/'
+  base: '/entrega-final-react-miguel-casas-v2/'
 })
